ad9108018d8a89274d9843c2ce35e9afbae75e57
Abhijit Poojari <49238918+AbhijitPoojari@users.noreply.github.com>
2020-09-06 17:42:20 +0530

    Add project files.

---
+++ EmployeeManagement/Migrations/20200905100616_AddPhotoPathColumn.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace EmployeeManagement.Migrations
+{
+    public partial class AddPhotoPathColumn : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "PhotoPath",
+                table: "Employees",
+                nullable: true);
+
+            migrationBuilder.UpdateData(
+                table: "Employees",
+                keyColumn: "Id",
+                keyValue: 1,
+                columns: new[] { "Email", "Name" },
+                values: new object[] { "john@tesla.com", "John" });
+
+            migrationBuilder.UpdateData(
+                table: "Employees",
+                keyColumn: "Id",
+                keyValue: 2,
+                columns: new[] { "Email", "Name" },
+                values: new object[] { "jason@johnson.com", "Jason" });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "PhotoPath",
+                table: "Employees");
+
+            migrationBuilder.UpdateData(
+                table: "Employees",
+                keyColumn: "Id",
+                keyValue: 1,
+                columns: new[] { "Email", "Name" },
+                values: new object[] { "mark@test.com", "Mark" });
+
+            migrationBuilder.UpdateData(
+                table: "Employees",
+                keyColumn: "Id",
+                keyValue: 2,
+                columns: new[] { "Email", "Name" },
+                values: new object[] { "jack@johnson.com", "Jack" });
+        }
+    }
+}
